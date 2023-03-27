0c626d29e721e6d637e55c459efabdf48f8e2fed
Michael Zhao <michaelzhao314@gmail.com>
2023-03-27 13:40:25 -0500

    Updated email body and subject

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -19,8 +19,8 @@ async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
   const msg: sendgrid.MailDataRequired = {
     to: email,
     from: process.env.SENDGRID_SENDER as string,
-    subject: 'Axxess Hackathon QR-code ID',
-    text: "Hello,\n\nThank you for registering for the Axxess hackathon at UTD! Below is your unique QR-code for scan-ins such as check-in, events, food, and more!\n\nWe can't wait for you to join us. Happy hacking!\n\nBest,\nAxxess",
+    subject: 'Axxess Hackathon QR Code',
+    text: "Hello,\n\nThank you for registering for the Axxess Hackathon. Below is your unique QR-code for check-in, swag, and food! We recommend arriving at 8:45am to get in line for check-in as space is limited.\n\nYou'll recieve an email in a few days (Wednesday or Thursday) with all the Axxess Hackathon event details.\n\nHackathon check-in begins at April 1st, 9 a.m. CDT.\n\nLocation:\nECSW 1.100 Axxess Atrium\n800 W. Campbell Road, Richardson, Texas 75080\n\nIf you have any questions or concerns, please don't hesitate to reach out to us at hackathon@axxess.com.\n\nThank you again for registering for the Axxess Hackathon. We can't wait to see you there!\n\nBest regards,\n\nThe Axxess Hackathon Team",
     attachments: [
       {
         content: qrcode,
@@ -32,9 +32,9 @@ async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({});
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    sendEmail(req, res);
+    await sendEmail(req, res);
   } else {
     res.status(405).json({});
   }
